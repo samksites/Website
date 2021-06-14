f97a813c97a4edb7431330d181990ad89844e24d
Sam <samuelksites@gmail.com>
2021-06-14 17:15:58 -0400

    compleated structure of website

--- js/colors.js
+++ js/colors.js
@@ -1,38 +1,30 @@
-var colors = ["#eb6b34","#7aeb34","#34ebc3","#c934eb","#eb3462"]
+var colors = ["#eb6b34","#7aeb34","#34ebc3","#c934eb","#eb3462","#ebdb34","#10deb8","#2e22e0","#ac24f0"]
 
 function boxes(number){
 
+    var tempColor =colors[Math.floor(Math.random() * 9)];
     switch(number){
-
+        
         case 0:
-            var tempColor =colors[Math.floor(Math.random() * 5)];
+            
             document.getElementById("one").style.color = tempColor;
             document.getElementById("one").style.textShadow = "1px 1px 5px " + tempColor;
             break;
 
-        case 1:
-            document.getElementById("one").style.color = "#fff";
-            document.getElementById("one").style.textShadow = "none";
-            break;
+       
 
         case 2:
-            var tempColor =colors[Math.floor(Math.random() * 5)];
+           
             document.getElementById("two").style.color = tempColor;
             document.getElementById("two").style.textShadow = "1px 1px 5px " + tempColor;
             break;
-        case 3:
-            document.getElementById("two").style.color = "#fff";
-            document.getElementById("two").style.textShadow = "none";
-            break;
+       
         case 4:
-            var tempColor =colors[Math.floor(Math.random() * 5)];
+           
             document.getElementById("three").style.color = tempColor;
             document.getElementById("three").style.textShadow = "1px 1px 5px " + tempColor;
             break;
-        case 5:
-            document.getElementById("three").style.color = "#fff";
-            document.getElementById("three").style.textShadow = "none";
-            break;
+        
 
 
     }
